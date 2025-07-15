3d83347d780a7ba4389606600ee9121ca6b1ea99
Your Name <you@example.com>
2025-07-15 15:05:01 +0300

    Final Fix: Hardcode port to 8080

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="/home/site/wwwroot"
-exec gunicorn --bind=0.0.0.0:$WEBSITES_PORT --workers=4 --timeout 600 whatsapp_bot:app
+exec gunicorn --bind=0.0.0.0:8080 --workers=4 --timeout 600 whatsapp_bot:app
